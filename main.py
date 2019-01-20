fb6e02e237ca47d20341003172ddb6e65e3c552d
zahramajd <zahra.majdabadi95@gmail.com>
2019-01-20 13:44:10 +0330

    TODO

---
+++ main.py
@@ -0,0 +1,8 @@
+
+#TODO: document collection
+#TODO: preprocessing
+#TODO: binary vector representation
+#TODO: topic discovery
+#TODO: topic incidence Matrix 
+#TODO: temporal topic correlation
+#TODO: topic trend forecasting
