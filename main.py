624cf3074cd0d08e7fe13e3031adca59b9837354
zahramajd <zahra.majdabadi95@gmail.com>
2019-01-20 23:35:08 +0330

    generate topic incidence matrix

--- main.py
+++ main.py
@@ -26,22 +26,40 @@ def bag_of_word_doc():
     return
 
 #TODO: topic discovery
+def topic_discovery():
+    return
+
 # assume that each doc contains keywords
-def topic_discovery(docs_per_year):
+def generate_topic_incidence_matrix(docs_per_year):
+
+    topic_incidence_matrix = {}
+    topics = set()
     docs_topic_per_year = docs_per_year
+
     for year in docs_topic_per_year:
         for i in range(len(docs_per_year[year])):
+            # tokenized is actually an array of the topics of a doc
             tokenized = docs_topic_per_year[year][i].split('\n')
             docs_topic_per_year[year][i]= []
             for t in tokenized:
-                docs_topic_per_year[year][i].append(t.replace(' ','_'))
+                topic = t.replace(' ','_')
+                docs_topic_per_year[year][i].append(topic)
+                
+                if not topic in topics:
+                    topics.add(topic)
+                    topic_incidence_matrix[topic] = {}
 
-    return docs_topic_per_year
+                if not year in topic_incidence_matrix[topic]:
+                    topic_incidence_matrix[topic][year] = 0
 
-#TODO: topic incidence matrix 
-def generate_topic_incidence_matrix():
+                topic_incidence_matrix[topic][year] = topic_incidence_matrix[topic][year]+1
 
-    return
+    for topic in topic_incidence_matrix:
+            for year in docs_topic_per_year:
+                if not year in topic_incidence_matrix[topic]:
+                    topic_incidence_matrix[topic][year] = 0
+
+    return docs_topic_per_year, topics, topic_incidence_matrix
 
 #TODO: temporal topic correlation
 def generate_topic_correlation():
@@ -52,5 +70,4 @@ def topic_forecast():
     return
 
 docs_per_year = read_documents()
-docs_topic_per_year = topic_discovery(docs_per_year)
-topic_incidence_matrix = generate_topic_incidence_matrix(docs_topic_per_year)
+topic_incidence_matrix, topics, topic_incidence_matrix = generate_topic_incidence_matrix(docs_per_year)
