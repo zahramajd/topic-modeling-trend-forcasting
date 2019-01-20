a791b4845987b5e296c77e1d211ef4c5e59ef85c
zahramajd <zahra.majdabadi95@gmail.com>
2019-01-20 19:35:04 +0330

    read documents

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
+import glob
+import os
 
-#TODO: document collection
+def read_documents():
+
+    docs_per_yaer = {}
+    path = 'documents'
+
+    for folder in os.listdir(path):
+        files=glob.glob(path+'/'+folder+'/*.txt')
+        docs_per_yaer[folder]=[]
+        for file in files:
+            f=open(file, 'r')  
+            c =f.read() 
+            docs_per_yaer[folder].append(c)
+            f.close() 
+    
+    return docs_per_yaer
+
 #TODO: preprocessing
+def normalize():
+    return
+
 #TODO: binary vector representation
+def bag_of_word_doc():
+    return
+
 #TODO: topic discovery
-#TODO: topic incidence Matrix 
+def topic_discovery():
+    return
+
+#TODO: topic incidence matrix 
+def generate_topic_incidence_matrix():
+    return
+
 #TODO: temporal topic correlation
+def generate_topic_correlation():
+    return
+
 #TODO: topic trend forecasting
+def topic_forecast():
+    return
+
+docs_per_yaer = read_documents()
