b885eed076e283f17e55ea7220f528c155e7838a
zahramajd <zahra.majdabadi95@gmail.com>
2019-01-20 20:39:19 +0330

    topic discovery

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import os
 
 def read_documents():
 
-    docs_per_yaer = {}
+    docs_per_year = {}
     path = 'documents'
 
     for folder in os.listdir(path):
         files=glob.glob(path+'/'+folder+'/*.txt')
-        docs_per_yaer[folder]=[]
+        docs_per_year[folder]=[]
         for file in files:
             f=open(file, 'r')  
             c =f.read() 
-            docs_per_yaer[folder].append(c)
+            docs_per_year[folder].append(c)
             f.close() 
     
-    return docs_per_yaer
+    return docs_per_year
 
 #TODO: preprocessing
 def normalize():
@@ -26,11 +26,21 @@ def bag_of_word_doc():
     return
 
 #TODO: topic discovery
-def topic_discovery():
-    return
+# assume that each doc contains keywords
+def topic_discovery(docs_per_year):
+    docs_topic_per_year = docs_per_year
+    for year in docs_topic_per_year:
+        for i in range(len(docs_per_year[year])):
+            tokenized = docs_topic_per_year[year][i].split('\n')
+            docs_topic_per_year[year][i]= []
+            for t in tokenized:
+                docs_topic_per_year[year][i].append(t.replace(' ','_'))
+
+    return docs_topic_per_year
 
 #TODO: topic incidence matrix 
 def generate_topic_incidence_matrix():
+
     return
 
 #TODO: temporal topic correlation
@@ -41,4 +51,6 @@ def generate_topic_correlation():
 def topic_forecast():
     return
 
-docs_per_yaer = read_documents()
+docs_per_year = read_documents()
+docs_topic_per_year = topic_discovery(docs_per_year)
+topic_incidence_matrix = generate_topic_incidence_matrix(docs_topic_per_year)
