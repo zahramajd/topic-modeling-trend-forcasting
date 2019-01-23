0b7ca5ee2328157cf1390b9e1e6ae17bf58ea5ff
zahramajd <zahra.majdabadi95@gmail.com>
2019-01-23 21:11:07 +0330

    support vector regression added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVR
 import numpy as np
 
 def read_documents():
@@ -71,7 +72,6 @@ def generate_topic_incidence_matrix(docs_per_year):
 def generate_topic_correlation():
     return
 
-#TODO: linear regression
 #TODO: support vector regression
 #TODO: ensemble
 
@@ -84,16 +84,14 @@ def topic_forecast(topic_incidence_matrix):
 
     def linear_regression(topic_year_actual):
         topic_year_predicted = {}
-
         years = []
         years_ = []
-        for year in topic_year_actual:
 
+        for year in topic_year_actual:
             if len(years)>1 :
                 regr = linear_model.LinearRegression()
                 regr.fit(np.array(years), np.array([topic_year_actual[k] for k in years_ if k in topic_year_actual]))
                 topic_year_predicted[year] = regr.predict(np.array([[float(year)]]))
-
 
             years.append([float(year)])
             years_.append(year)
@@ -102,13 +100,25 @@ def topic_forecast(topic_incidence_matrix):
 
     def support_vector_regression(topic_year_actual):
         topic_year_predicted = {}
+        years = []
+        years_ = []
+        
+        for year in topic_year_actual:
+            if len(years)>1 :
+                clf = SVR(gamma='scale', C=1.0, epsilon=0.2)
+                clf.fit(np.array(years), np.array([topic_year_actual[k] for k in years_ if k in topic_year_actual]))
+                topic_year_predicted[year] = clf.predict(np.array([[float(year)]]))
+
+            years.append([float(year)])
+            years_.append(year)
+
         return topic_year_predicted
 
     def ensemble(topic_year_actual):
         topic_year_predicted = {}
         return topic_year_predicted
     
-    topic_year_predicted = linear_regression(topic_incidence_matrix['Machine_learning_algorithms'])
+    topic_year_predicted = support_vector_regression(topic_incidence_matrix['Machine_learning_algorithms'])
     plot_topic_year(topic_incidence_matrix['Machine_learning_algorithms'],topic_year_predicted)
     
     return
@@ -116,4 +126,4 @@ def topic_forecast(topic_incidence_matrix):
 docs_per_year = read_documents()
 topic_incidence_matrix, topics, topic_incidence_matrix = generate_topic_incidence_matrix(docs_per_year)
 topic_forecast(topic_incidence_matrix)
-# plt.show()
+plt.show()
